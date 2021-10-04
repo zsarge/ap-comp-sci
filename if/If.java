eed83d9454daa5075df6507f08d6ff2de9094c5b
zsarge <zackarywsargent+github@gmail.com>
2021-10-04 08:33:33 -0400

    multi way selection

--- if/If.java
+++ if/If.java
@@ -1,21 +1,22 @@
 import static java.lang.System.out;
+import java.util.Scanner;
 
 class If {
 	public static void main(String[] args) {
-		/*
-		 * boolean coming = false;
-		 * out.println("My aunt and uncle are " + (coming ? "" : "NOT ") + "coming.");
-		 */
 
-		int classRank = 1;
-		int act = 36;
+		Scanner input = new Scanner(System.in);
 
-		if (false)
-			if (act >= 30 || classRank <= 10)
-				out.println("Welcome to Wassamatta U!");
-			else
-				out.println("Too bad :( - No Wassamatta U for you.");
+
+		out.print("Enter your class rank: ");
+		int classRank = input.nextInt();
+		out.print("Enter your ACT score: ");
+		int act = input.nextInt();
+
+		if (act >= 34)
+			out.println("Welcome to Wassamatta U with honors!");
+		else if (act >= 30 || classRank <= 10)
+			out.println("Welcome to Wassamatta U!");
 		else
-			out.println("No admissions");
+			out.println("No Wassamatta U for you :(");
 	}
 }
