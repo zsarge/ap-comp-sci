894501aeec202f7052cb30d9a3a57af41281171c
zsarge <zackarywsargent+github@gmail.com>
2021-10-12 12:11:48 -0400

    add timer to prime calculations

--- prime/MathStuff.java
+++ prime/MathStuff.java
@@ -1,14 +1,49 @@
 class MathStuff {
 	public static void main(String[] args) {
-		for (int i = 0; i < 100; i++)
-			if (isPrime(i))
-				System.out.println(i);
+		Timer timer = new Timer();
+		double ot = 0.0;
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			// timer.start();
+			// boolean l = isPrimeLong(i);
+			// timer.stop();
+			// double lt = timer.getTime();
+
+			timer.start();
+			boolean o = isPrimeOptimized(i);
+			timer.stop();
+			ot = timer.getTime();
+
+			if (ot > 0.0) {
+				System.out.print("I: " + i);
+				// System.out.print(" Long: " + lt);
+				System.out.println(" Op: " + ot);
+				break;
+			}
+
+			if ( false) {
+				System.out.print(i);
+				// System.out.print("\tisPrimeLong = " + l);
+				System.out.println("\tisPrimeOptimized = " + o);
+			}
+		}
 	}
 
-	public static boolean isPrime(int n) {
+	public static boolean isPrimeLong(int n) {
 		for (int i = 2; i < n; i++)
 			if (n % i == 0)
 				return false;
+		return true;
+	}
+
+	public static boolean isPrimeOptimized(int n) {
+		if (n % 2 == 0 && n != 2 && n != 0)
+			return false;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0)
+				return false;
+			// if (n % (i * i) == 0)
+				// return false;
+		}
 		return true;
 	}
 }
