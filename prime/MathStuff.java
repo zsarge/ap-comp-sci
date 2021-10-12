950a0bb3d33f06c9d7e827e8613740c370831674
zsarge <zackarywsargent+github@gmail.com>
2021-10-12 08:26:35 -0400

    add timer class

---
+++ prime/MathStuff.java
@@ -0,0 +1,14 @@
+class MathStuff {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++)
+			if (isPrime(i))
+				System.out.println(i);
+	}
+
+	public static boolean isPrime(int n) {
+		for (int i = 2; i < n; i++)
+			if (n % i == 0)
+				return false;
+		return true;
+	}
+}
