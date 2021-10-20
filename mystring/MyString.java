685c9b607d40a27ae57e5e40122f6f2bfa808fd6
zsarge <zackarywsargent+github@gmail.com>
2021-10-20 14:12:55 -0400

    add replace function

--- mystring/MyString.java
+++ mystring/MyString.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
 class MyString {
 	public static void main(String[] args) {
 		String test1 = "Hello World!";
 		String test2 = "Hello John!";
 		int index = 3;
+
 		test(test1.substring(4), substring(test1, 4));
 		test(test1.substring(4, 7), substring(test1, 4, 7));
 		test(test1.compareTo(test2), compareTo(test1, test2));
@@ -11,6 +14,13 @@ class MyString {
 		test(test1.indexOf("ello"), indexOf(test1, "ello"));
 		test(test1.indexOf("wllo"), indexOf(test1, "wllo"));
 		test(test1.indexOf("orld"), indexOf(test1, "orld"));
+		test(test1.length(), length(test1));
+		test("".length(), length(""));
+		test(test1.replace("l", "w"), replace(test1, "l", "w"));
+		test(test1.replace("ll", "ww"), replace(test1, "ll", "ww"));
+		test(test1.replace("", ""), replace(test1, "", ""));
+		test(test1.replace("World", ""), replace(test1, "World", ""));
+		test(test1.replace(" ", "_"), replace(test1, " ", "_"));
 	}
 	public static String substring(String str, int start) {
 		StringBuilder sb = new StringBuilder();
@@ -49,6 +59,28 @@ class MyString {
 		}
 		return -1;
 	}
+	public static int length(String str) {
+		char[] chars = str.toCharArray();
+		int len = 0;
+		for (char c : chars)
+			len++;
+		return len;
+	}
+	public static String replace(String base, String find, String replacer) {
+		if (find.equals("") || base.equals(""))
+			return base;
+		int index = base.indexOf(find);
+		if (index == -1)
+			return base; // base case for recursion
+		StringBuilder sb = new StringBuilder();
+		sb.append(base.substring(0, index));
+		sb.append(replacer);
+		int endOfReplaceStr = find.length() + index ;
+		int start = endOfReplaceStr > base.length()
+			? base.length() : endOfReplaceStr;
+		sb.append(base.substring(start));
+		return replace(sb.toString(), find, replacer);
+	}
 	/* ---------------- */
 	public static void test(String a, String b) {
 		System.out.print(a + " == " + b + ": ");
@@ -66,10 +98,10 @@ class MyString {
 
 /*
  *
- *         length
  *           replace
  *             toLowerCase
  * Done:
+ *         length
  *       indexOf
  * substring
  *   compareTo
